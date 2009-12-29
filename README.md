b697d58376ac612b07af59d96a88ad6573188ac6
Arieh <arieh.glazer@gmail.com>
2009-12-29 23:43:15 +0200

    first commit

--- README.md
+++ README.md
@@ -1,15 +1,56 @@
 HistoryManager
 ========
-![Screenshot]()
+This class is intended to supply a back-button functionality to JS actions. It is also a way to implement an Observer pattern in JS. 
+To supply the back button functionality i've ported the detection methods from digitarald's HistoryManager, which is built on top of Moo 1.1.
 
+Tested With: FF3.5, Safari 4.0.4, Chrome 3 and IE8. IE6-7 work as well, but require double-click on the back button to acualy go back (no idea why...)
 
 How to use
 ----------
+The class is quite simple. it is basicly a domain event listner. 
+It can monitor and modify the values of assigned parameters, and fire events when thier values are modified.
+First, we create an Instance:
 
+    #JS
+	var hm = new HistoryManager;
+	
+Next, lets say we want to follow an action called PageNum. Atatching its events will look like this:
+   
+    #JS
+    hm.addEvent('PageNum-added',function(value){
+        console.log('a new Page Number:'+value);
+    });
+   
+    hm.addEvent('PageNum-changed',function(value){
+        console.log('Page Number changed:'+value);
+    });
+    
+    hm.addEvent('PageNum-removed',function(value){
+        console.log('a Page Number was removed:'+value);
+    });
+
+Now we can start the service:
+    
+    #JS
+    hm.start();	
+	
+We can now assign the values of PageNum, firing its events and registring the changes to the browser history:
+
+    #JS
+	hm.set('PageNum',1); //will fire PageNum-added with value of 1
+	hm.set('PageNum',2); //will fire PageNum-chenged with value of 2
+	hm.remove('PageNum'); //will fire PageNum-removed with value of 2
+
+The set method can recieve any JSON compatible value. 
 
 Options
 ---------
+  * iframeSrc : an alternative source for an iframe file
+  * start : whether to start service on creation (default:false)
 
 Events
 -------
-
+The class's events are not pre-defined, but concice as such:
+  * '-added' : will fire when a new value is added. will send the new value to the function
+  * '-changed' : will fire when a value is modified. will send the new value to the function
+  * '-removed' : will fire when a value is removed. will send the last value to the function
--- Source/HistoryManager.js
+++ Source/HistoryManager.js
@@ -41,11 +41,147 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE 
 */
 var HistoryManager = new Class({
-	Implements : [Options],
-	options : {
-		
+	Implements : [Options,Events],
+	options: {
+		iframeSrc: 'blank.html',
+		start : false
 	},
+	isFirst :false,
+	handle : $empty,
+	iframe : null,
+	list : new Hash({}),
+	needIframe : ((Browser.Engine.trident && Browser.Engine.version <6 )),
+	currentHash : '',
 	initialize : function(options){
+		var list = this.getHash(),self=this;
+		if (list) this.list = new Hash(JSON.decode(list));
+		if (this.options.start) this.start();
+	},
+	
+	observe: function() {
+		if (this.timeout) return;
+		var state = this.getState();
 		
+		if (this.state == state) return;
+		
+		if ((this.needIframe || Browser.Engine.webkit && Browser.Engine.version<=419) && (this.state !== null)) this.setState(true);
+		else this.state = state;
+		this.checkHash(state);		
+	},
+	
+	checkHash : function(hash){
+		var jhash = new Hash(JSON.decode(hash)), self = this;
+		jhash.each(function(value,name){
+			self.set(name,value);
+		});
+		if (jhash.getLength()<this.list.getLength()) this.list.each(function(value,name){
+			if (false === jhash.has(name)){
+				self.remove(name,self.list);
+			}
+		});
+		this.currentHash = hash;
+	},
+	
+	set : function(name,value){
+		if (this.list.has(name)){
+			this.list.set(name,value);
+			this.fireEvent(name+'-changed',value);
+		}else{
+			this.dontCheck = true;
+			this.list.set(name,value);
+			this.fireEvent(name+'-added',value);
+		}
+		this.setState();
+	},
+	
+	remove : function(name,list){
+		var list = (list) ? list : this.list ,
+			value = list.get(name);
+		list.erase(name);
+		this.fireEvent(name+'-removed',value);
+		this.setState();
+	},
+	
+	generateState: function() {
+		return this.list.toJSON();
+	},
+
+	update: function() {
+		if (!this.started) return this;
+		var state = this.generateState();
+		if ((!this.state && !state) || (this.state == state)) return this;
+		this.setState(state);
+		return this;
+	},
+
+	observeTimeout: function() {
+		if (this.timeout) this.timeout = $clear(this.timeout);
+		else this.timeout = this.observeTimeout.delay(200, this);
+	},
+
+	getHash: function() {
+		var href = decodeURIComponent(top.location.href);
+		var pos = href.indexOf('#') + 1;
+		return (pos) ? href.substr(pos) : '';
+	},
+
+	getState: function() {
+		var state = this.getHash();
+
+		if (this.iframe) {
+			var doc = this.iframe.contentWindow.document;
+			if (doc && doc.body.id == 'state') {
+				var istate = doc.body.innerText;
+				if (this.state == state) return istate;
+				this.istateOld = true;
+			} else return this.istate;
+		}
+		if (Browser.Engine.webkit && Browser.Engine.version<=419 && history.length != this.count) {
+			this.count = history.length;
+			return (this.states[this.count - 1]===undefined) ? this.states[this.count-1] :  state;
+		}
+		return state;
+	},
+
+	setState: function(fix) {
+		state = this.generateState();
+		
+		if (Browser.Engine.webkit && Browser.Engine.version<=419) {
+			if (!this.form) this.form = new Element('form', {method: 'get'}).inject(document.body);
+			this.count = history.length;
+			this.states[this.count] = state;
+			this.observeTimeout();
+			this.form.setProperty('action', '#' + state).submit();
+		} else top.location.hash = state || '#';
+		
+		if (this.needIframe && (!fix || this.istateOld)) {
+			if (!this.iframe) {
+				this.iframe = new Element('iframe', {
+					src: this.options.iframeSrc,
+					style: 'visibility: hidden;'
+				}).inject(document.body);
+				this.istate = this.state;
+			}
+			try {
+				var doc = this.iframe.contentWindow.document;
+				doc.open();
+				doc.write('<html><body id="state">' + state + '</body></html>');
+				doc.close();
+				this.istateOld = false;
+			} catch(e) {};
+		}
+		
+		this.state = state;
+	},
+	stop : function(){$clear(this.handle); this.started = false;},
+	start : function(){
+		var self=this;
+
+		this.setState();
+		this.list.each(function(value,name){
+			self.fireEvent(name+'-added',value);
+		});
+		this.handle = this.observe.bind(this).periodical(100);
+		this.started = true;
 	}
-})
+});
